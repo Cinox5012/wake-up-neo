48dd084d3d82c44747c443ef40e86a7a374b9e87
Cinox5012 <82369780+Cinox5012@users.noreply.github.com>
2022-01-09 23:18:20 +0100

    Create wake-up-neo.sh

---
+++ wake-up-neo.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+clear
+echo -e "\e[32m W \e[0m"
+sleep 0.3
+clear
+echo -e "\e[32m Wa \e[0m"
+sleep 0.3
+clear
+echo -e "\e[32m Wak \e[0m"
+sleep 0.3
+clear
+echo -e "\e[32m Wake \e[0m"
+sleep 0.3
+clear
+echo -e "\e[32m Wake u \e[0m"
+sleep 0.3
+clear
+echo -e "\e[32m Wake up \e[0m"
+sleep 0.3
+clear
+echo -e "\e[32m Wake up, \e[0m"
+sleep 0.3
+clear
+echo -e "\e[32m Wake up, N \e[0m"
+sleep 0.3
+clear
+echo -e "\e[32m Wake up, Ne \e[0m"
+sleep 0.3
+clear
+echo -e "\e[32m Wake up, Neo \e[0m"
+sleep 0.3
+clear
+echo -e "\e[32m Wake up, Neo. \e[0m"
+sleep 0.3
+clear
+echo -e "\e[32m Wake up, Neo.. \e[0m"
+sleep 0.3
+clear
+echo -e "\e[32m Wake up, Neo... \e[0m"
